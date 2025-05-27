9db27bfd60f12c19895309082271c5d0bd0f3343
Mark Wissink <markcwissink@gmail.com>
2025-05-27 11:49:56 -0400

    Add label to dep array

--- package/src/Autocomplete.tsx
+++ package/src/Autocomplete.tsx
@@ -67,7 +67,7 @@ export const Autocomplete = <
         slotProps={{ input: InputProps, inputLabel: InputLabelProps }}
       />
     ),
-    [placeholder],
+    [placeholder, label],
   );
 
   return (
