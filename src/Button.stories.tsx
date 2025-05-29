02caf0808327d414c866d0bf48643d4bd99b619c
Mark Wissink <markcwissink@gmail.com>
2025-05-29 15:23:26 -0400

    Add Button story

---
+++ src/Button.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "@mui/material";
+
+const meta = {
+  title: "Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const BaseButton: Story = {
+  args: {
+    children: "Label",
+    variant: "contained",
+  },
+};
