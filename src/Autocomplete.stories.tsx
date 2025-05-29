298c62bdbcd3e1694a81aca0e9ae0ad7df16c70b
Mark Wissink <20484462+mcwissink@users.noreply.github.com>
2025-05-29 10:31:23 -0400

    Restructure project to combine storybook and package (#6)

--- src/Autocomplete.stories.tsx
+++ src/Autocomplete.stories.tsx
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Autocomplete } from "@mybldr/ui";
+import { Autocomplete } from "./Autocomplete";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: "Autocomplete",
   component: Autocomplete,
